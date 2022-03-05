693826b70040f40c7a5be00dffc1011a4f98c7ae
khadiza21 <bibikhadiza474@gmail.com>
2022-03-05 08:43:44 +0600

    learning source

---
+++ 7-eventLoop.js
@@ -0,0 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/EventLoop
+// https://www.cloudflare.com/learning/dns/what-is-dns/
+// https://youtu.be/8aGhZQkoFbQ
+// https://youtu.be/pHquof7coLM
